d4ca2e0fa04921c6876f007a1c56189e07730d09
Ethan Lane <ethantroublelane@gmail.com>
2026-01-23 13:56:58 -0500

    implemented insertation logic to simple db

--- server/app-modules/config.ts
+++ server/app-modules/config.ts
@@ -43,14 +43,61 @@ class manager {
 
     }
 }
+import fs from 'fs/promises';
 /**
  * Custom minimal database
  */
 class db {
     database: string;
-    file: File;
-    constructor(database: string, file: File) {
-        this.database = database;
-        this.file = file;
+    payload: string;
+    constructor(database: string) {
+        this.database = `./${database}`; // make database a path
+        this.payload = "";
+        
+        
+    }
+    public async init_schema() {
+        fs.appendFile(this.database, ""); // create file if not exists
+        const line = new entry("logger", "db_log", "init");
+        await this.store_entry(line);
+    }
+    public async store_entry(line: entry) {
+        // load file
+        await this.loadFile();
+        // insertition logic
+        this.payload = this.payload.replaceAll(line.as_string(), ""); // remove dups
+        this.payload += line.as_string(); // add entry
+        // commit changes
+        await this.commit_entry();
+    }
+    private async commit_entry() {
+        await fs.writeFile(this.database, this.payload);
+    }
+    private async loadFile() {
+        this.payload = await fs.readFile(this.database).then((data) => data.toString());
+        console.log(this.payload)
     }
 }
+/**
+ * Entry format helper for db class
+ */
+class entry {
+    id: string;
+    table: string;
+    data: string;
+    constructor(id: string, table: string, data: string) {
+        this.id = id;
+        this.table = table;
+        this.data = data;
+    }
+    public as_string() {
+        return `<${this.id}:${this.table}:${this.data.replace(" ", "***")}>\n`;
+    }
+}
+async function main() {
+    const a = new db('./database.db');
+    a.init_schema();
+    const x = new entry("app", "installs", "data2s");
+    await a.store_entry(x);
+}
+main();
