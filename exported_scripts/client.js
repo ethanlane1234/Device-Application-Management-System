8927f953a79a230bda68e2352af785fee6c3f7da
Ethan Lane <ethantroublelane@gmail.com>
2026-01-26 02:08:23 -0500

    removed outdated duplicate files

--- exported_scripts/client.js
+++
@@ -1,7 +0,0 @@
-import { client } from "../app.js";
-
-
-const PORT = 45697; // should be one less than server port
-// client
-const client_device = new client(PORT, "localhost", "localhost");
-const http_server = client_device.listen(); // get the underlying http server
